26b28989fa8276510927175d0738557b3b9b271b
alxqkekrroqkfwk <fexxet@icloud.com>
2023-08-23 23:36:28 +0900

    init

--- src/test/java/com/team6/finalproject/FinalProjectApplicationTests.java
+++ src/test/java/com/team6/finalproject/FinalProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.team6.finalproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FinalProjectApplicationTests {
 
     @Test
