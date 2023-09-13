a9141975ac58eb59dc7e5f4cec2be4c804667336
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-09-14 04:36:50 +0900

    [update] 모임-상세 페이지에 UI 개선 및 상호작용 추가
    - '수정하기' 버튼 기능 구현, /meeting-update로 리다이렉트.
    - '수정하기'와 '삭제하기' 버튼을 모임 작성자에게만 표시.
    - 각 member-card에 클릭 이벤트 추가, 사용자의 프로필로 리다이렉트.
    - 더 나은 사용자 경험을 위해 member-card에 포인터 커서 및 focus/active 효과 개선.

--- src/main/java/com/team6/finalproject/meeting_user/dto/MeetingUsersResponseDto.java
+++ src/main/java/com/team6/finalproject/meeting_user/dto/MeetingUsersResponseDto.java
@@ -8,11 +8,13 @@ import java.util.List;
 
 @Getter
 public class MeetingUsersResponseDto {
+    private Long userId;
     private String nickname;
     private String introduction;
     private String profileImage;
 
     public MeetingUsersResponseDto(MeetingUser meetingUser) {
+        this.userId = meetingUser.getUser().getId();
         this.nickname = meetingUser.getUser().getProfile().getNickname();
         this.introduction = meetingUser.getUser().getProfile().getIntroduction();
         this.profileImage = meetingUser.getUser().getProfile().getProfileImage();
