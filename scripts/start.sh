c00384f1785adcef905c94670af51294ef64e218
canseeit <leekw1245@gmail.com>
2023-09-16 19:33:02 +0900

    [update] 배포 자동화 코드 수정 - 빌드 시 plain.jar 파일 생성하지 않도록 build.gradle 설정 추가 - deploy.yml JDK 세팅, 빌드 버전 수정 - start.sh, stop.sh JAR_FILE 파일명 수정 - start.sh jar 파일 실행 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 PROJECT_ROOT="/home/ubuntu/app"
-PROJECT_NAME="Final-Project"
-JAR_FILE="$PROJECT_ROOT/${PROJECT_NAME}-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -18,10 +17,7 @@ chmod +x $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar \
-    -Dspring.config.location=/var/www/html/spring/src/main/resources/application.properties,/var/www/html/spring/src/main/resources/application.yml \
-    -Dspring.profiles.active=real \
-    $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
