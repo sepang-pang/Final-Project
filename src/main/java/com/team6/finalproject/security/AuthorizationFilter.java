30109a6a21f84d66373f1f953ffa292c4b30e24c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-09-16 12:14:52 +0900

    [refactor] 오타 수정

--- src/main/java/com/team6/finalproject/security/AuthorizationFilter.java
+++ src/main/java/com/team6/finalproject/security/AuthorizationFilter.java
@@ -69,7 +69,7 @@ public class AuthorizationFilter extends OncePerRequestFilter {
 
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String[] excludePath = {"/signup", "/login", "/user/reissue", "/aa", "/kakao/callback", "/api/sms","/api/findid","/api/findpassword","/api/users/id-auth",
-                "/api/users/id-inquiry","/api/resetpassword","/api/users/resetpassword", "/sub-main", "/api가/clubs/get"};
+                "/api/users/id-inquiry","/api/resetpassword","/api/users/resetpassword", "/sub-main", "/api/clubs/get"};
 
         String path = request.getRequestURI();
         return Arrays.stream(excludePath).anyMatch(path::startsWith);
